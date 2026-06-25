9d187293f88e15bb5c1e3384b58f91aabdf39fc4
iamjosephmj <iamjosephmj@gmail.com>
2026-06-25 23:23:12 +0200

    test(plugin): assert library attribution + unknown-fallback in proposal extraction

--- poseidon-gradle-plugin/src/test/kotlin/tech/ssemaj/poseidon/gradle/ProposalExtractionTest.kt
+++ poseidon-gradle-plugin/src/test/kotlin/tech/ssemaj/poseidon/gradle/ProposalExtractionTest.kt
@@ -17,5 +17,22 @@ class ProposalExtractionTest {
         val props = GeneratePolicyTask.extractProposals(manifest)
         assertEquals(2, props.size)
         assertEquals("telemetry.sdk.com", props[0].host)
+        assertEquals("com.foo.sdk", props[0].library)
+        assertEquals("crash.sdk.com", props[1].host)
+    }
+
+    @Test fun defaultsLibraryToUnknownWhenToolsNodeAbsent() {
+        val manifest = """
+            <manifest package="com.app">
+              <application>
+                <meta-data android:name="tech.ssemaj.poseidon.proposes"
+                           android:value="solo.sdk.com"/>
+              </application>
+            </manifest>
+        """.trimIndent()
+        val props = GeneratePolicyTask.extractProposals(manifest)
+        assertEquals(1, props.size)
+        assertEquals("solo.sdk.com", props[0].host)
+        assertEquals("unknown", props[0].library)
     }
 }
